ae9181e3dfdfabd80d243f18b2d9dc977b106f16
namigop <erik.araojo@wcfstorm.co>
2015-12-21 22:56:15 +0800

    adding assemblyinfo.fs

---
+++ WcfStorm.Tala/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace WcfStorm.Tala
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<AssemblyTitle("WcfStorm.Tala")>]
+[<AssemblyDescription("")>]
+[<AssemblyConfiguration("")>]
+[<AssemblyCompany("WcfStorm Solutions")>]
+[<AssemblyProduct("WcfStorm.Tala")>]
+[<AssemblyCopyright("Copyright WcfStorm Solutions ©  2015")>]
+[<AssemblyVersion("1.1.0.0")>]
+[<AssemblyFileVersion("1.1.0.0")>]
+// [<assembly:AssemblyKeyFileAttribute(@"wcfstormapp-public.snk")>]
+// [<assembly:AssemblyDelaySignAttribute(true)>]
+do ()
